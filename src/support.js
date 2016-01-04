2e13fcec81bc9d99a973d2a44016f4b15accf6aa
Eric Lanehart <pushred@gmail.com>
2016-01-04 15:16:05 -0500

    Disable MS touch support for now
    General touch movement triggering scroll events

--- src/support.js
+++ src/support.js
@@ -5,7 +5,7 @@ module.exports = (function getSupport() {
         hasWheelEvent: 'onwheel' in document,
         hasMouseWheelEvent: 'onmousewheel' in document,
         hasTouch: 'ontouchstart' in document,
-        hasTouchWin: navigator.msMaxTouchPoints && navigator.msMaxTouchPoints > 1,
+        hasTouchWin: false,
         hasPointer: !!window.navigator.msPointerEnabled,
         hasKeyDown: 'onkeydown' in document,
         isFirefox: navigator.userAgent.indexOf('Firefox') > -1
